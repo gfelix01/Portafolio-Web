98e6149f32de7bbfc5e7b0760217c11930e35d51
Your Name <you@example.com>
2023-07-31 14:07:19 -0400

    Create function js mixerProjects

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*================Mixup Filter================*/
+let mixerProjects =mixitup('.projects__container', {
+    selectors: {
+        target: '.project__item'
+    },
+    animation: {
+        duration: 300
+    }
+});
