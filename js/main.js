73b142fe87e072c8423cee083adcc33f5f8220bd
Your Name <you@example.com>
2023-08-01 21:26:26 -0400

    add Mixup filter

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
+
 /*================Mixup Filter================*/
-let mixerProjects =mixitup('.projects__container', {
+let mixerProjects = mixitup('.projects__container', {
     selectors: {
-        target: '.project__item'
+        target: '.project__item',
     },
     animation: {
         duration: 300
-    }
+    },
 });
+/*Active Work*/
+const linkWork = document.querySelectorAll('.category__btn');
+
+function activeWork() {
+    linkWork.forEach((a) => a.classList.remove('active-work')); 
+    this.classList.add('active-work');
+} 
+  
+linkWork.forEach ((a) => a.addEvenListener('click', activeWork));
