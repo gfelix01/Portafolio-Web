6127b5d3e69a45141208fe55a774d42e26188ad6
Gabriel Felix <gabriel_arturo01@hotmail.com>
2023-09-06 16:15:38 -0400

    Add function contact  form email send

--- javascript/main.js
+++ javascript/main.js
@@ -34,7 +34,36 @@ var testiSwiper = new Swiper('.testimonial__container ', {
   mousewheel: true,
   keyboard: true,
 });
+/*================Contact Form================*/
+const contactForm = document.getElementById('contact-form'),
+contactName =  document.getElementById('contact-name'),
+contactEmail = document.getElementById('contact-email'),
+Message = document.getElementById('message'),
+contactMessage = document.getElementById('contact-message');
 
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  if (contactName.value ==='' || contactEmail.value ===  ''|| Message.value ===''){
+    contactMessage.classList.remove('color-light');
+    contactMessage.classList.add('color-dark');
+
+    contactMessage.textContent = 'Write all the inputs fields';
+  }else {
+    emailjs.sendForm('service_iannqf9','template_l7gl5qv','#contact-form','g5yS7kFZ6s_wl4Y6b')
+    .then(()=> {
+      contactMessage.classList.add('color-light');
+      contactMessage.textContent =  'Message sent✅👌🏾';
+
+
+      setTimeout(() => {
+        contactMessage.textContent = '';
+      },5000);
+    });
+  }
+};
+
+contactForm.addEventListener('submit',sendEmail);
 
 
   
