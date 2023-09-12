dd199f50ceca8237855682a51962d52258f4a0c1
Gabriel Felix <gabriel_arturo01@hotmail.com>
2023-09-12 10:37:14 -0400

    format codes

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,19 @@
 /*================ Mixup Filter ================*/
 const mixerProjects = mixitup('.projects__container', {
-  selectors: {
-      target: '.project__item',
-  },
-  animation: {
-      duration: 300
-  },
+    selectors: {
+        target: '.project__item',
+    },
+    animation: {
+        duration: 300
+    },
 });
 
 /* Active Work */
 const linkWork = document.querySelectorAll('.category__btn');
 
 function setActiveWork() {
-  linkWork.forEach((a) => a.classList.remove('active-work'));
-  this.classList.add('active-work');
+    linkWork.forEach((a) => a.classList.remove('active-work'));
+    this.classList.add('active-work');
 }
 
 linkWork.forEach((a) => a.addEventListener('click', setActiveWork));
@@ -21,16 +21,16 @@ linkWork.forEach((a) => a.addEventListener('click', setActiveWork));
 /*================ Testimonial Swiper ================*/
 // Initialize Swiper carousel
 const testiSwiper = new Swiper('.testimonial__container', {
-  loop: true,
-  navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-      el: ".swiper-pagination",
-  },
-  mousewheel: true,
-  keyboard: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
 });
 
 /*================ Contact Form ================*/
@@ -41,44 +41,44 @@ const message = document.getElementById('message');
 const contactMessage = document.getElementById('contact-message');
 
 const sendEmail = (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  if (contactName.value === '' || contactEmail.value === '' || message.value === '') {
-      contactMessage.classList.remove('color-light');
-      contactMessage.classList.add('color-dark');
+    if (contactName.value === '' || contactEmail.value === '' || message.value === '') {
+        contactMessage.classList.remove('color-light');
+        contactMessage.classList.add('color-dark');
 
-      contactMessage.textContent = 'Please fill in all input fields.';
-  } else {
-      emailjs.sendForm('service_iannqf9', 'template_l7gl5qv', '#contact-form', 'g5yS7kFZ6s_wl4Y6b')
-          .then(() => {
-              contactMessage.classList.add('color-light');
-              contactMessage.textContent = 'Message sent ✅👌🏾';
+        contactMessage.textContent = 'Please fill in all input fields.';
+    } else {
+        emailjs.sendForm('service_iannqf9', 'template_l7gl5qv', '#contact-form', 'g5yS7kFZ6s_wl4Y6b')
+            .then(() => {
+                contactMessage.classList.add('color-light');
+                contactMessage.textContent = 'Message sent ✅👌🏾';
 
-              const successMessage = document.getElementById('success-message');
+                const successMessage = document.getElementById('success-message');
 
-              if (successMessage) {
-                  successMessage.style.display = 'block';
-              }
+                if (successMessage) {
+                    successMessage.style.display = 'block';
+                }
 
-              contactForm.style.display = 'none';
+                contactForm.style.display = 'none';
 
-              setTimeout(() => {
-                  contactMessage.textContent = '';
+                setTimeout(() => {
+                    contactMessage.textContent = '';
 
-                  if (successMessage) {
-                      successMessage.style.display = 'none';
-                  }
+                    if (successMessage) {
+                        successMessage.style.display = 'none';
+                    }
 
-                  contactForm.style.display = 'block';
-              }, 5000);
-          })
-          .catch(function (error) {
-              contactMessage.classList.remove('color-light');
-              contactMessage.classList.add('color-dark');
-              contactMessage.textContent = 'Error sending message. Please try again later.';
-              console.error('Error sending email:', error);
-          });
-  }
+                    contactForm.style.display = 'block';
+                }, 5000);
+            })
+            .catch(function (error) {
+                contactMessage.classList.remove('color-light');
+                contactMessage.classList.add('color-dark');
+                contactMessage.textContent = 'Error sending message. Please try again later.';
+                console.error('Error sending email:', error);
+            });
+    }
 };
 
 contactForm.addEventListener('submit', sendEmail);
